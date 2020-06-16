745754d0f6845da0c55d9694f475bacc8762babc
Avyuktaj <avjp5929@gmail.com>
2020-06-16 23:16:48 +0530

    pro md

---
+++ 10 digit to word w14 part2.py
@@ -0,0 +1,6 @@
+n=int(input("enter a digit from 0 to 10:")
+numberlist=["zero","one","two","three",
+      "four","five","six","seven","eight","nine","ten"]
+      print(numberlist[n])
+      
+      
