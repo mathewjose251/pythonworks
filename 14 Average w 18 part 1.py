c54b193faf8001d4d210ecc3c30e31b7ac6c30b8
Avyuktaj <avjp5929@gmail.com>
2020-06-23 23:10:26 +0530

    pro made on 15 june

---
+++ 14 Average w 18 part 1.py
@@ -0,0 +1,23 @@
+#print("Enter 'x' for exit.")
+print("Enter any 3 numbers to find its average: ")
+
+number1= int(input("FIRST NO.:"))
+
+number2= int(input("SECOND NO.:"))
+
+number3= int(input("THIRD NO.:"))
+
+#if n1 == 'x':
+    #print(sum)
+#else:
+    #number1 = int(n1)
+    #number2 = int(n2)
+    #number3 = int(n3)
+
+sum = number1 + number2 + number3
+
+average = sum/3
+
+print("Average of entered 3 numbers is ", average )
+print("****************************************************************************************")
+
